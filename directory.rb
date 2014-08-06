94267552086f7f732a6b40f6ec947c91982b730b
Henry Stanley <henry@henrystanley.com>
2014-08-06 17:19:27 +0100

    Further refactoring, removing comments

--- directory.rb
+++ directory.rb
@@ -2,18 +2,14 @@
 
 def interactive_menu
 	loop do
-		# Attempt to load the list of students from file
 		try_load_students
-		# Print a menu of options for the user
 		print_menu
-		# Get input from the user and pass it to 'process' method which selects an option
 		process(STDIN.gets.chomp)
 	end
 end
 
 def process(selection)
-	# Takes the option from the user and chooses the appropriate method
-	case selection.downcase
+	case selection
 	when "1" then input_students
 	when "2" then show_students
 	when "3" then save_students
@@ -25,7 +21,6 @@ def process(selection)
 end
 
 def print_menu
-	# Prints the menu
 		puts "1. Input students"
 		puts "2. Show students"
 		puts "3. Save students"
@@ -35,7 +30,6 @@ def print_menu
 end
 
 def show_students
-	# Shows the current list of students in memory
 	print_header
 	print_students_list
 	print_footer
@@ -44,32 +38,36 @@ end
 def input_students
 	puts "Please enter the names, cohort and hobby of the students."
 	puts "To finish, just make a blank entry."
-	# get the first name
-	name = STDIN.gets.chomp
-	cohort = STDIN.gets.chomp.downcase.to_sym ||= :august
-	hobby = STDIN.gets.chomp
-	# while the name is not empty, repeat this code
-	until name.empty? do
-		
-		# add the student hash to the array
-		add_students(name, cohort, hobby)
+	get_input
+	until @name.empty? do
+		add_students(@name, @cohort, @hobby)
 		puts "Now we have #{@students.length} student#{"s" if @students.length >= 2}"
-		# get another name from the user
-		name = STDIN.gets.chomp
-		cohort = STDIN.gets.chomp.downcase.to_sym ||= :august
-		hobby = STDIN.gets.chomp
+		get_input
 	end
 end
 
+def get_input
+	@name = STDIN.gets.chomp
+	@cohort = STDIN.gets.chomp ||= :august # ensures default value of :august
+	@hobby = STDIN.gets.chomp
+end
+
 def print_header
-	# Prints header for the list of students
 	puts "The students of the August cohort at Makers Academy:"
 end
 
+def load_students(filename = 'students.csv')
+	File.open(filename, "r") do |file|
+		file.readlines.each do |line|
+			name, cohort, hobby = line.chomp.split(',')
+			add_students(name, cohort, hobby)
+		end
+	end
+	puts "Students loaded from file!"
+end
+
 def save_students
-	# open the file for writing
 	file = File.open("students.csv", "w")
-	#iterate over students
 	@students.each do |student|
 		student_data = [student[:name], student[:cohort], student[:hobby]]
 		csv_line = student_data.join(",")
@@ -77,17 +75,6 @@ def save_students
 	end
 	puts "Students saved!"
 	file.close
-end
-
-def load_students(filename = 'students.csv')
-	# Loads a list of students from a csv file. If no filename is specified, defaults to students.csv
-	File.open(filename, "r") do |file|
-		file.readlines.each do |line|
-		name, cohort, hobby = line.chomp.split(',') # Reads a line of the .csv file, removes the trailing line break, splits at the comma and saves each chunk into three variables
-		add_students(name, cohort, hobby) # Passes those variables to the add_students method, which saves them to the array
-		end
-	end
-	puts "Students loaded from file!"
 end
 
 def add_students(name, cohort, hobby)
@@ -99,20 +86,18 @@ def print_students_list
 end
 
 def try_load_students
-	# Attempts to load a list of students from a command line argument, if one was given
-	filename = ARGV.first #1st argument from cmd line
-	return if filename.nil? #get out if no filename
-	if File.exists?(filename) #if it exists
+	filename = ARGV.first
+	return if filename.nil?
+	if File.exists?(filename)
 		load_students(filename)
 		puts "Loaded #{@students.length} from #{filename}"
-	else #if it doesn't exist
+	else
 		puts "Sorry, #{filename} doesn't exist."
 		exit
 	end
 end
 
 def print_footer
-	# Prints footer for use after the list of students
 	puts "Overall, we have #{@students.length} great students."
 end
 
